cc09726e0bd4442d829dbe178d7f88b6acebb8aa
2161357 <2161357@my.ipleiria.pt>
2018-12-12 17:20:15 +0000

    Initial implementations

--- AcademicManagement-ejb/build/generated-sources/ap-source-output/entities/Course_.java
+++ AcademicManagement-ejb/build/generated-sources/ap-source-output/entities/Course_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-10T21:40:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-12T17:14:56")
 @StaticMetamodel(Course.class)
 public class Course_ { 
 
--- AcademicManagement-ejb/build/generated-sources/ap-source-output/entities/Teacher_.java
+++ AcademicManagement-ejb/build/generated-sources/ap-source-output/entities/Teacher_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-10T21:40:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-12T17:14:56")
 @StaticMetamodel(Teacher.class)
 public class Teacher_ extends User_ {
 
